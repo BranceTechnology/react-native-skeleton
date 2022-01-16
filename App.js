e50d1892504c587b8c8a5462b9df3d642083f9dc
Jimmy Bastos <jimmybastosdev@gmail.com>
2022-01-16 19:30:30 -0300

    🎉 First commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Box, NativeBaseProvider} from 'native-base';
+import {StyleSheet, useColorScheme} from 'react-native';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <NativeBaseProvider>
+      <Box>Hello world</Box>
+    </NativeBaseProvider>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
