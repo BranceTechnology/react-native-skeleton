d9b8a3b5b5e44aae339cf8d7095dab3e02306b9e
Jimmy Bastos <jimmybastosdev@gmail.com>
2022-01-16 19:49:56 -0300

    🎨 Add project structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,44 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-};
+  env: {
+    es2021: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+    ecmaVersion: 12,
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['@typescript-eslint', 'unused-imports', 'react', 'prettier'],
+  extends: [
+    '@react-native-community',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
+  ],
+
+  rules: {
+    'prettier/prettier': 'error',
+    'react/prop-types': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    '@typescript-eslint/no-unused-expressions': 2,
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-unused-vars': 'off',
+    'no-unused-expressions': 'off',
+    'no-use-before-define': 'off',
+    'unused-imports/no-unused-imports-ts': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_'
+      }
+    ]
+  }
+}
